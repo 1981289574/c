670f294aa99b88059618bdc90eae4fe47e0680f3
1981289574 <86427568+1981289574@users.noreply.github.com>
2021-07-20 12:06:32 +0800

    使用for循环计算5个数的平均数

---
+++ byte_7.20.1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main ()
+{
+  int s = 0;
+  int i = 0;
+  int sun = 0;
+  for(i=0;i<5;i++0）
+      {
+        scanf("%d",&s);
+        sum += s;
+      }
+      printf("%.1f\n",sum/5.0);
+      return 0;
+}
+  
+  
