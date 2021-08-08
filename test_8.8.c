662dd640cd22e0b9ec4e1d56d800535818af6d20
1981289574 <86427568+1981289574@users.noreply.github.com>
2021-08-08 23:54:42 +0800

    BC 13 Ascll码值

---
+++ test_8.8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[] = {73,32,99,97,110,32,100,111,32,105,116,33};
+    int i;
+    for(i=0;i<12;i++)
+    {
+        printf("%c",a[i]);
+    }
+    printf("\n");
+    return 0;
+}
