f1921d3ac7e180f3fc329e02cb3b98328335ad14
1981289574 <86427568+1981289574@users.noreply.github.com>
2021-08-31 23:04:00 +0800

    test_8.31

---
+++ BC41 你是天才吗？.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a = 0;
+    while(~scanf("%d",&a))
+    {
+        if(a>=140)
+        {
+            printf("Genius");
+        }
+    }
+    return 0;
+}
