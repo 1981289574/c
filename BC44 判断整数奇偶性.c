6dcbf1a25f4035099900807d515c7a080f43b33b
1981289574 <86427568+1981289574@users.noreply.github.com>
2021-09-03 13:27:30 +0000

    test_9.3

---
+++ BC44 判断整数奇偶性.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+    int n = 0;
+    while(scanf("%d",&n) != EOF)
+    {
+        if(n%2==0)
+        {
+            printf("Even\n");
+        }
+        else
+        {
+            printf("Odd\n");
+        }
+    }
+    return 0;
+}
