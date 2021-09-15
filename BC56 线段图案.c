dae4d158a4a547a5b851cb1cddf849b6ebcf4459
1981289574 <86427568+1981289574@users.noreply.github.com>
2021-09-15 23:17:14 +0800

    Create BC56 线段图案.c

---
+++ BC56 线段图案.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int i,n;
+    while(scanf("%d",&n)!=EOF)
+    {
+        for(i=0;i<n;i++)
+        {
+            printf("*");
+    
+        }
+        printf("\n");
+    }
+}
