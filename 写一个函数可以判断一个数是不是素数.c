459016637b2eafe4aa98656e1ca2371b30de99ae
1981289574 <86427568+1981289574@users.noreply.github.com>
2021-10-31 21:40:33 +0800

    Create 写一个函数可以判断一个数是不是素数.c

---
+++ 写一个函数可以判断一个数是不是素数.c
@@ -0,0 +1,26 @@
+//写一个函数可以判断一个数是不是素数
+int is_prime(int n)
+{
+	int j = 0;
+	for (j = 2; j <= sqrt(n); j++)
+	{
+		if (n % j == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int count = 0;
+	int i = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		if (is_prime(i) == 1)
+		{
+			printf("%d\n", i);
+			count++;
+		}
+	}
+	printf("count=%d\n", count);
+	return 0;
+}
